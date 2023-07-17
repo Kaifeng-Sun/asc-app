1895f568bdbcf078e54e533afa1ef3c3c28d0d13
Kaifeng-Sun <skf951130@gmail.com>
2023-07-17 10:31:07 +1000

    refector: update the nav component

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <title>Welcome MyCharity</title>
+        <title>MyCharity</title>
       </Head>
     </>
   );
