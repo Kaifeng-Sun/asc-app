7af64e909b27beaa851ab17b43c7dba0e5bd46a0
Kaifeng-Sun <skf951130@gmail.com>
2023-08-04 13:16:04 +1000

    add structure

---
+++ src/pages/aboutaisa/structure.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function structure() {
+  return (
+    <>
+      <div>OUR GOVERNANCE STRUCTURE</div>
+      <div>In line with normal practices for an incorporated association, AISA’s activities on a day-to-day basis are governed by a Management Committee, which is elected by a majority vote by the members of the Association at its Annual General Meeting. The Management Committee is made up of occupants of the positions of President, Vice-President, Secretary and Treasurer, and six committee members.</div>
+    </>
+  );
+}
+
+export default structure;
