e7747170f6686611644ec08774080ac7df3a4c7c
Kaifeng-Sun <skf951130@gmail.com>
2023-08-04 13:08:44 +1000

    fix: mission

--- src/pages/aboutaisa/mission.tsx
+++ src/pages/aboutaisa/mission.tsx
@@ -5,10 +5,10 @@ function mission() {
     <>
       <div>mission</div>
       <div>
-        AISA’s mission is to organise a variety of activities and events that
+        AISA's mission is to organise a variety of activities and events that
         benefit members' physical and mental health and keep them socially
         engaged within the community and suitably informed about the topics of
-        relevance to them. AISA’S modus operandi is Mutual Help, guided by
+        relevance to them. AISA'S modus operandi is Mutual Help, guided by
         skilled volunteers from within the community, giving their time for the
         benefit of members with sincerity, integrity, and commitment.
       </div>
