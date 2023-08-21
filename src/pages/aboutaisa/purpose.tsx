677b038717fd875e76a184b042a53019d7b488a0
saritatamang <srtatmg@gmail.com>
2023-08-21 14:37:20 +1000

    updated about aisa navigation bar

--- src/pages/aboutaisa/purpose.tsx
+++ src/pages/aboutaisa/purpose.tsx
@@ -1,15 +1,32 @@
+import PageTitle from "@/src/components/Template/PageTitle";
 import React from "react";
+import { Box, Center, Text, Grid, GridItem, Image } from "@chakra-ui/react";
 
 function Purpose() {
   return (
     <>
-      <div>Purpose</div>
-      <div>
-        AISA’s purpose is to promote positive ageing and general welfare of
-        Indian Seniors living in Victoria, encourage active lifestyles amongst
-        them, and enhance their connectedness within the community and the
-        broader Australian society.
-      </div>
+      <PageTitle title="Our Purpose" />
+      <Box h="300px">
+        <Center>
+          <Grid templateColumns="repeat(2, 1fr)" gap={2}>
+            <GridItem w="100%">
+              <Text
+                fontSize="2xl"
+                maxH="300px"
+                overflowY={{ base: "scroll", sm: "scroll", md: "visible" }}
+              >
+                AISA’s purpose is to promote positive ageing and general welfare of
+                Indian Seniors living in Victoria, encourage active lifestyles amongst
+                them, and enhance their connectedness within the community and the
+                broader Australian society.
+              </Text>
+            </GridItem>
+            <GridItem w="100%">
+              <Image alt="" src="/aisalogo.png" maxH="300px" />
+            </GridItem>
+          </Grid>
+        </Center>
+      </Box>
     </>
   );
 }
